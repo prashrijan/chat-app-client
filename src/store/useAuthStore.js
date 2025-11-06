6ac25cdf52619615617d0568fde740d444d043d4
Prashrijan <shresthaprashrijan@gmail.com>
2025-11-07 09:26:38 +1100

    put online users in auth store

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -8,6 +8,7 @@ export const useAuthStore = create((set) => ({
     isSigningIn: false,
     isUpdatingProfile: false,
     isAuthChecking: true,
+    onlineUsers: [],
 
     checkAuth: async () => {
         try {
